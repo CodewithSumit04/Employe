ab3e602be233b00ecce7e3356d437adf14e58f3a
Sumit <suumroiy@gmail.com>
2026-01-16 17:11:00 +0530

    Update employe-routes.js

--- github/routes/employe-routes.js
+++ github/routes/employe-routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const EmployeController = require('../controller/employe-controller');
+const AttendenceController = require('../controller/attendence')
 const router = express.Router();
 router.get('/Employerdetails', EmployeController.GetEmploye);
 router.post('/Employerdetails/delete', EmployeController.DeleteEmploye);
 router.put('/Employerdetails', EmployeController.PutEmploye);
 router.post('/Employerdetails', EmployeController.PostEmploye);
+router.post("/attendence", AttendenceController.Postattendence);
+router.get("/attendence", AttendenceController.Getattendence);
+
 module.exports = router;
